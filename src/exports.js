8d46690c83fcb7caeb3c9d4273b7b042c3c6ee45
nVitius <mauricio@nviti.us>
2016-07-17 14:15:49 -0700

    Export all classes and constants for module

---
+++ src/exports.js
@@ -0,0 +1,27 @@
+import {
+  ARG_REQUIRED,
+  ARG_OPTIONAL,
+  ARG_ARRAY
+} from './argument';
+
+import {
+  OPT_REQUIRED,
+  OPT_OPTIONAL,
+  OPT_ARRAY,
+  OPT_EMPTY
+} from './option';
+
+import Command from './command';
+
+export default {
+  ARG_REQUIRED,
+  ARG_OPTIONAL,
+  ARG_ARRAY,
+
+  OPT_REQUIRED,
+  OPT_OPTIONAL,
+  OPT_ARRAY,
+  OPT_EMPTY
+};
+
+export { Command as Command };
