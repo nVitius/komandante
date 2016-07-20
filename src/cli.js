6fc31db78e3e33ad4bf8d533a8ab6e4835bfcff4
nVitius <mauricio@nviti.us>
2016-07-20 11:31:48 -0700

    Parse args/opts for a Command and run it

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,13 @@
 import Debug from 'debug'
+import Minimist from 'minimist';
+
+import Command from './command';
 import Loader from './loader';
 
-import Ordre from './exports';
 
 const debug = new Debug('ordre:cli:');
 
-export default class CLI {
+class CLI {
   /**
    * @param {String} root
    * @param {String} dir
@@ -13,16 +15,37 @@ export default class CLI {
   constructor(root, dir) {
     debug(`Creating CLI in ${root} under ${dir}`);
 
+    /**
+     * @type {Loader}
+     * @name CLI#_loader
+     * @private
+     */
     this._loader = new Loader(root, dir);
 
     /**
-     * @type {Array <Ordre.Command>}
+     * @type {Array<Command>}
+     * @name CLI#_commands
+     * @private
      */
     this._commands = this._loader.commands;
+
+    /**
+     * @type {Object}
+     * @name CLI#_cliArgs
+     * @private
+     */
+    this._cliArgs = Minimist(process.argv.slice(3)); // 2 is the command name
+    debug(this._cliArgs);
+
+    /**
+     * @type {Command}
+     * @name CLI#_activeCommand
+     * @private
+     */
+    this._activeCommand = null;
   }
 
   /**
-   *
    * @param {String} name Name of the command to run.
    *                      Defaults to first node argument. (i.e. node cli.js <b>foo:say</b>)
    */
@@ -33,6 +56,62 @@ export default class CLI {
       throw new Error('Command does not exist!');
     }
 
-    this._commands[name].run();
+    this._activeCommand = this._commands[name];
+    this._parse();
+
+    this._activeCommand.run();
+  }
+
+  _parse() {
+    if (this._activeCommand === null) {
+      throw new Error('No active command');
+    }
+
+    // TODO: loop over arguments and assign values
+    let argCount = 0;
+
+    // parse all arguments
+    for (let name in this._activeCommand.arguments) {
+      if(!this._activeCommand.arguments.hasOwnProperty(name)) continue;
+
+      const argument = this._activeCommand.arguments[name];
+
+      let value;
+      if (argument.isArray()) {
+        value = this._cliArgs._.slice(argCount);
+
+      } else {
+        value = this._cliArgs._[argCount];
+
+      }
+
+      debug(`Assign value ${value} to argument ${argument.name}`);
+      argument.value = value;
+      argCount++;
+    }
+
+    for (let name in this._activeCommand.options) {
+      if(!this._activeCommand.options.hasOwnProperty(name)) continue;
+
+      const option = this._activeCommand.options[name];
+
+      let value;
+      if (option.name in this._cliArgs) {
+        value = this._cliArgs[option.name];
+
+      } else if (option.shortcut in this._cliArgs) {
+        value = this._cliArgs[option.shortcut];
+
+      } else {
+        return;
+
+      }
+
+      debug(`Asssigning value ${value} to option ${option.name}`);
+      option.value = value;
+    }
+
   }
 }
+
+export default CLI;
